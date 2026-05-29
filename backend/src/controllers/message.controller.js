9a01919af5a818e106e75b092f823d956f82c75d
Abhishek1187 <158825804+Abhishek1187@users.noreply.github.com>
2026-05-30 03:21:29 +0530

    api's working

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import Message from "../models/Message.js";
+import cloudinary from "../lib/cloudinary.js";
 
 
 export const getAllContacts = async (req, res) => {
@@ -21,8 +22,8 @@ export const getMessagesByUserId = async(req, res) => {
 
         const messages = await Message.find({
             $or: [
-                {senderId:myId ,  recieverId: userToChatId},
-                {senderId:userToChatId, recieverId: myId},
+                {senderId:myId ,  receiverId: userToChatId},
+                {senderId:userToChatId, receiverId: myId},
             ]
         })
 
@@ -40,18 +41,7 @@ export const sendMessage = async(req , res) => {
      try {
     const { text, image } = req.body;
     const { id: receiverId } = req.params;
-    const senderId = req.user._id;
-
-    if (!text && !image) {
-      return res.status(400).json({ message: "Text or image is required." });
-    }
-    if (senderId.equals(receiverId)) {
-      return res.status(400).json({ message: "Cannot send messages to yourself." });
-    }
-    const receiverExists = await User.exists({ _id: receiverId });
-    if (!receiverExists) {
-      return res.status(404).json({ message: "Receiver not found." });
-    }
+    const senderId = req.user._id;p   
 
     let imageUrl;
     if (image) {
@@ -66,13 +56,9 @@ export const sendMessage = async(req , res) => {
       text,
       image: imageUrl,
     });
+    await newMessage.save()
 
-    await newMessage.save();
-
-    const receiverSocketId = getReceiverSocketId(receiverId);
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
-    }
+    
 
     res.status(201).json(newMessage);
   } catch (error) {
@@ -81,31 +67,5 @@ export const sendMessage = async(req , res) => {
   }
 };
 
-export const getChatPartners = async (req, res) => {
-  try {
-    const loggedInUserId = req.user._id;
 
-    // find all the messages where the logged-in user is either sender or receiver
-    const messages = await Message.find({
-      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
-    });
-
-    const chatPartnerIds = [
-      ...new Set(
-        messages.map((msg) =>
-          msg.senderId.toString() === loggedInUserId.toString()
-            ? msg.receiverId.toString()
-            : msg.senderId.toString()
-        )
-      ),
-    ];
-
-    const chatPartners = await User.find({ _id: { $in: chatPartnerIds } }).select("-password");
-
-    res.status(200).json(chatPartners);
-  } catch (error) {
-    console.error("Error in getChatPartners: ", error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
 
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { getAllContacts , getMessagesByUserId , sendMessage , getChatPartners} from "../controllers/message.controller.js";
+import { getAllContacts , getMessagesByUserId , sendMessage , } from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/contacts", protectRoute, getAllContacts);
-router.get("/chats" , protectRoute , getChatPartners);
+
+router.get("/contacts",protectRoute ,getAllContacts);
+//router.get("/chats"  , protectRoute , getChatPartners);
 router.get("/:id" , protectRoute , getMessagesByUserId);
-router.post("/send/:id" , protectRoute , sendMessage);
+router.post("/send/:id" , protectRoute, sendMessage);
+
+
 export default router;
